e65ad331c8e7dddcb0614a4efb10fa03b5045505
David Sarria <sarriafreelancer@gmail.com>
2024-03-12 14:11:49 -0500

    fin de sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { CounterModule } from './modules/counter/counter.module';
+import { DbzModule } from './modules/dbz/dbz.module'
+import { HeroesModule } from './modules/heroes/heroes.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CounterModule,
+    HeroesModule,
+    DbzModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
